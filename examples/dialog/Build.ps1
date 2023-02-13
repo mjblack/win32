aedbfd8bc728ff58b99eb42058870170ade0f466
Matthew J. Black <mjblack@gmail.com>
2023-02-13 09:29:38 -0600

    Add testing for dialog box example

---
+++ examples/dialog/Build.ps1
@@ -0,0 +1,8 @@
+﻿Install-Module VSSetup -Scope CurrentUser -Force
+$cwd = Get-Location
+$vsinstpath = Get-VSSetupInstance -All | Select-VSSetupInstance -Latest | Select-Object  -ExpandProperty InstallationPath
+& "${vsinstpath}\Common7\Tools\Launch-VsDevShell.ps1"
+cd $cwd\examples\dialog
+rc resource.rc
+$lopts = "/LIBPATH:${cwd}\examples\dialog resource.res"
+crystal build -o dialogbox1.exe .\window.cr --link-flags "${lopts}"
